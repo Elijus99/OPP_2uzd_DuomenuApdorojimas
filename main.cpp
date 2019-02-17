b39bb78e4634794d26aeaca6b8b551ca8c1ab514
Elijus99 <47322512+Elijus99@users.noreply.github.com>
2019-02-17 15:17:34 +0200

    Added reading from .txt files

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <algorithm>
 #include <cfloat>
+#include <fstream>
 
 using std::cin;
 using std::string;
@@ -24,7 +25,9 @@ struct stud {
 
 void ivedimas(char &GType, std::vector<stud> &ls, int &VSize, int &PSize);
 void rikiavimas(std::vector<int> x[], int n);
+double galutinis(double egzaminas, char GType, int n, std::vector<int> nd);
 void isvedimas(std::vector<stud> ls, int VSize, int PSize, char GType);
+bool exist(const char *fileName);
 
 int main()
 {
@@ -55,18 +58,7 @@ void ivedimas(char &GType, std::vector<stud> &ls, int &VSize, int &PSize)
 {
 	bool valid_input = false;
 	int N;
-
-	do {
-		cout << "Keliu studentu duomenis norite irasyti?" << endl;
-		cin >> N;
-		if (!(valid_input = cin.good())) {
-			cout << "That input is invalid!" << endl;
-			cin.clear();
-			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		}
-	} while (!valid_input);
-	cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
+	char choice;
 	do {
 		cout << "Ka norite naudoti galutinio balo skaiciavimui: vidurki ar mediana? (Iveskite 'v' arba 'm')" << endl;
 		cin >> GType;
@@ -80,13 +72,70 @@ void ivedimas(char &GType, std::vector<stud> &ls, int &VSize, int &PSize)
 		}
 	} while (GType != 'v' || GType != 'V' || GType != 'M' || GType != 'm');
 
-	double vidurkis, egzaminas, mediana;
-	char choice;
+	do {
+		cout << "Ar norite nuskaityti duomenis is failo 'kursiokai.txt'? (y/n)" << endl;
+		cin >> choice;
+		if (choice == 'y' || choice == 'Y' || choice == 'N' || choice == 'n') {
+			break;
+		}
+		else {
+			cout << "That input is invalid!" << endl;
+			cin.clear();
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+	} while (choice != 'y' || choice != 'Y' || choice != 'N' || choice != 'n');
+	const char *fileName = "kursiokai.txt";
+	if (choice == 'y' || choice == 'Y') {
+		if (exist(fileName)) {
+			std::ifstream in(fileName);
+			in.ignore(10000, '\n');
+			string vard, pav;
+			std::vector<int> nd;
+			double egzaminas, gal;
+			int n = 5, j=0;
+			int temp;
+			while (in >> vard >> pav) {
+				if (VSize < vard.size()) {
+					VSize = vard.size();
+				}
+				if (PSize < pav.size()) {
+					PSize = pav.size();
+				}
+				nd.clear();
+				for (int i = 0; i < n; i++)
+				{
+					in >> temp;
+					nd.push_back(temp);
+				}
+				in >> egzaminas;
+				gal = galutinis(egzaminas, GType, n, nd);
+				ls.push_back({ vard, pav, n, nd, gal });
+			}
+			cout << "---Is failo nuskaityti " << ls.size() << " studentu duomenys---" << endl;
+		}
+		else {
+			cout << "---Failas 'kursiokai.txt' nebuvo rastas---" << endl;
+		}
+	}
+	
+	do {
+		cout << "Keliu studentu duomenis norite irasyti?" << endl;
+		cin >> N;
+		if (!(valid_input = cin.good())) {
+			cout << "That input is invalid!" << endl;
+			cin.clear();
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+	} while (!valid_input);
+	cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+	double egzaminas;
 	int j, temp;
+	int SIZE = ls.size();
 	std::random_device rd;
 	std::mt19937 mt(rd());
 	std::uniform_int_distribution <int> gen(1, std::nextafter(10, DBL_MAX));
-	for (int i = 0; i < N; i++)
+	for (int i = SIZE; i < (N+SIZE); i++)
 	{
 		ls.push_back(stud());
 		cout << "Iveskite " << i + 1 << "-o studento varda" << endl;
@@ -168,41 +217,46 @@ void ivedimas(char &GType, std::vector<stud> &ls, int &VSize, int &PSize)
 			cout << "Egzamino rezultatas: " << egzaminas;
 			cout << endl;
 		}
+		ls[i].galutinis = galutinis(egzaminas, GType, ls[i].n, ls[i].nd);
 
-		vidurkis = 0;
-		for (int j = 0; j < ls[i].n; j++)
-		{
-			vidurkis += ls[i].nd[j];
-		}
-		if (ls[i].n == 0) {
-			vidurkis = 0;
-		}
-		else {
-			vidurkis /= ls[i].n;
-		}
-		std::sort(ls[i].nd.begin(), ls[i].nd.end());
-		if (ls[i].n > 0) {
-			if (ls[i].n % 2 == 1) {
-				mediana = ls[i].nd[ls[i].n / 2];
-			}
-			else {
-				mediana = (double)(ls[i].nd[ls[i].n / 2 - 1] + ls[i].nd[ls[i].n / 2]) / 2;
-			}
-		}
-		else {
-			mediana = 0;
-		}
-
-		if (GType == 'v') {
-			ls[i].galutinis = 0.4 * vidurkis + 0.6 * egzaminas;
-
-		}
-		else if (GType == 'm') {
-			ls[i].galutinis = 0.4 * mediana + 0.6 * egzaminas;
-
-		}
 		cout << endl;
 	}
+}
+
+double galutinis(double egzaminas, char GType, int n, std::vector<int> nd)
+{
+	double vidurkis = 0;
+	for (int j = 0; j < n; j++)
+	{
+		vidurkis += nd[j];
+	}
+	if (n == 0) {
+		vidurkis = 0;
+	}
+	else {
+		vidurkis /= n;
+	}
+	std::sort(nd.begin(), nd.end());
+	double mediana;
+	if (n > 0) {
+		if (n % 2 == 1) {
+			mediana = nd[n / 2];
+		}
+		else {
+			mediana = (double)(nd[n / 2 - 1] + nd[n / 2]) / 2;
+		}
+	}
+	else {
+		mediana = 0;
+	}
+	double galutinis;
+	if (GType == 'v' || GType == 'V') {
+		galutinis = 0.4 * vidurkis + 0.6 * egzaminas;
+	}
+	else if (GType == 'm' || GType == 'M') {
+		galutinis = 0.4 * mediana + 0.6 * egzaminas;
+	}
+	return galutinis;
 }
 
 void isvedimas(std::vector<stud> ls, int VSize, int PSize, char GType)
@@ -233,3 +287,8 @@ void isvedimas(std::vector<stud> ls, int VSize, int PSize, char GType)
 		cout << std::left << setw(VSize + 6) << ls[i].vard << setw(PSize + 7) << ls[i].pav << setprecision(3) << ls[i].galutinis << endl;
 	}
 }
+bool exist(const char *fileName)
+{
+	std::ifstream infile(fileName);
+	return infile.good();
+};
