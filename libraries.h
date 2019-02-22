25734c7d369e3c876468dc783fa5553876877725
Elijus99 <47322512+Elijus99@users.noreply.github.com>
2019-02-22 18:23:03 +0200

    Added libraries.h header file

---
+++ libraries.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef LIBRARIES_H_INCLUDED
+#define LIBRARIES_H_INCLUDED
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <string>
+using std::cin;
+using std::string;
+using std::endl;
+using std::cout;
+using std::setprecision;
+using std::setw;
+#endif
