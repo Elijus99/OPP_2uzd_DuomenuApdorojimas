ccad209617d1974ba100152ee71c363a0a8299b6
Elijus99 <47322512+Elijus99@users.noreply.github.com>
2019-04-05 14:40:49 +0300

    Added v0.5.1 vector version

---
+++ v0.5.1/v0.5.1vector/libraries.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef LIBRARIES_H_INCLUDED
+#define LIBRARIES_H_INCLUDED
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <string>
+#include <chrono>
+using std::cin;
+using std::string;
+using std::endl;
+using std::cout;
+using std::setprecision;
+using std::setw;
+#endif
---
+++ v0.5.1/v0.5.1vector/main.cpp
@@ -0,0 +1,23 @@
+#include "functions.h"
+#include "libraries.h"
+#include <algorithm>
+
+int main()
+{
+	int VSize = 10, PSize = 10;
+	string GType{};
+	std::vector<stud> ls;
+	std::chrono::duration<double> diff1;
+	ivedimas(GType, ls, VSize, PSize, diff1);
+	auto start = std::chrono::high_resolution_clock::now();
+	SortToGroups(ls);
+	auto end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> diff2 = end - start;
+	std::sort(ls.begin(), ls.end(), compareAlphabet);
+	isvedimas(ls, VSize, PSize, GType);
+	OutputToFiles(ls, GType, VSize, PSize);
+	cout << "Duomenu nuskaitymas is failu uztruko: " << std::fixed << setprecision(4) << diff1.count() << " s" << endl;
+	cout << "Studentu rusiavimas i grupes uztruko: " << std::fixed << setprecision(4) << diff2.count() << " s" << endl;
+	system("pause");
+	return 0;
+}
