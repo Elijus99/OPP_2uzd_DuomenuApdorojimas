6f37c095f73c4c8aa19b68cb3e7c1ce2f0b77562
Elijus99 <47322512+Elijus99@users.noreply.github.com>
2019-04-05 17:55:37 +0300

    Add files via upload

---
+++ v1.0.1 list/2strat/libraries.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef LIBRARIES_H_INCLUDED
+#define LIBRARIES_H_INCLUDED
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <string>
+#include <list>
+#include <chrono>
+using std::cin;
+using std::string;
+using std::endl;
+using std::cout;
+using std::setprecision;
+using std::setw;
+#endif
